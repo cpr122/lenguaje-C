67fffe6d5ec7ff2bc3c4726d7c021927457697ea
cpr122 <160140454+cpr122@users.noreply.github.com>
2024-02-22 19:47:42 -0600

    Create ejercicioIf.c

---
+++ ejercicioIf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdbool.h>
+int main(){
+
+int a;
+printf("Ingresa tu calificacion:");
+scanf("%i",&a);
+if (a>=8){
+
+ printf("felicidades aprobaste el curso");
+}else{
+
+printf(" no aprovaste");
+}
+}

fff397d8dbc7506f06ed62dfff48d16d28a337a6
cpr122 <160140454+cpr122@users.noreply.github.com>
2024-02-22 19:43:46 -0600

    Create if.c

---
+++ if.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdbool.h>
+int main(){
+    if(true){
+        printf("todo este \n");
+        printf("codigo se imprime dentro\n del bloque if\n");
+    }
+    int numero=55;
+    if (numero %2==0)
+    {
+       printf("el numero es par\n");
+
+    }
+    printf("fin de programa");
+    
+}

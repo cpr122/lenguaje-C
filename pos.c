5ed3fe7aad38911fd8c49110c093fbcebb8e32c8
cpr122 <160140454+cpr122@users.noreply.github.com>
2024-02-24 20:14:13 -0600

    Create pos.c

---
+++ pos.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main (){
+    int var=1;
+    int con=1;
+    //es equivale a var =var+1
+    printf("%d",var++);
+    printf("%d",var);
+    //Es equivale a con=con-1
+    printf("%d",con--);
+    printf("%d",con);
+}

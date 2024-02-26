da7f7b873ef79d78bb2e77569bc5fa4cc33decc7
cpr122 <160140454+cpr122@users.noreply.github.com>
2024-02-25 23:29:36 -0600

    Create recibirDatos.c

---
+++ recibirDatos.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (){
+    int a;
+printf("Ingresa tu edad:");
+scanf("%i",&a);
+printf("tu edad es: %i",a);
+int m = a*365*24*60*60;
+printf(" tu tiempo de vida es de: %i",m);
+
+}

ceaebe52aab0fdc9cc452c95c98e77a1efb865ef
cpr122 <160140454+cpr122@users.noreply.github.com>
2024-02-27 21:01:07 -0600

    Create laminas.c

---
+++ laminas.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (){
+    printf("Bienvenido Cristian ");
+    system("Pause");
+    return 0;
+
+}
+//lamina 2
+
+int main (){
+    // quiero imprimir el numero 3
+    printf("%d \n", 3);
+     // quiero imprimir la suma de 3 mas 4
+    printf("%d \n", 3+4);
+
+    printf("la suma de %d+%d es= %d \n ",3,4,3+4);
+    // en el metodo principal escribe el codigo que imprima la suma de : 5+8,78787+3259
+     printf("la suma de 5+8 :%d \n", 5+8);
+     printf("la suma de 78787+3259 :%d \n",78787+3259);
+
+}
+
+//lamina 3
+main (){
+    int unNumero=3;
+    int otroNumero=4;
+    //QUIERO IMPRIMIR LA SUMA 3+4
+printf("%d \n",unNumero+otroNumero);
+
+}

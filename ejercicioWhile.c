7946264cb726fe288b9c44a4d91f538c992c70d7
cpr122 <160140454+cpr122@users.noreply.github.com>
2024-02-28 18:35:44 -0600

    Create ejercicioWhile.c

---
+++ ejercicioWhile.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main (){
+    int i=1;
+
+while (i<=500){
+    int x2=i*i;
+    int x3=i*i*i;
+
+   printf("numero%d\ncuadrado%d\nnumero cubo %d\n",i,x2,x3);
+   i++; /* code */
+}
+return 0;
+}

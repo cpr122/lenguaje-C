ca01d0d8a66a0c826c1b0ae9b305d1580b0ba679
cpr122 <160140454+cpr122@users.noreply.github.com>
2024-03-03 22:48:04 -0600

    Create des.c

---
+++ des.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+    int descuento=10;
+    int descuentoC=20;
+    int cliente=0;
+    int z=0;
+    z=(cliente)?descuentoC:descuento;
+    printf("el decuento es de %d  \n",z);
+
+    printf("fin del programa");
+
+}

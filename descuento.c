449225577a1234dd03eb83fb744d463a37f53df5
cpr122 <160140454+cpr122@users.noreply.github.com>
2024-03-03 22:44:23 -0600

    Create descuento.c

---
+++ descuento.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (){
+    printf("Bienvenido Usuario\nAverigua si eres sujeto a un credito hipotecario\n");
+int a;
+int sueldo;
+printf("Ingresa tiempo en el que has estado en la empresa:");
+scanf("%i",&a);
+scanf("ingresa tu sueldo: %i",sueldo);
+
+int m = (sueldo*100)/10;
+}

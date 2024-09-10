b39c2401fa5c4c51606059817ce401d9eb056b04
cpr122 <160140454+cpr122@users.noreply.github.com>
2024-09-09 23:15:01 -0600

    Create Fac.c

---
+++ Fac.c
@@ -0,0 +1,16 @@
+
+
+1. Calcular el factorial de un número:
+```
+#include <stdio.h>
+
+int main() {
+    int numero, factorial = 1;
+    printf("Ingrese un numero: ");
+    scanf("%d", &numero);
+    for (int i = 1; i <= numero; i++) {
+        factorial *= i;
+    }
+    printf("El factorial de %d es: %d\n", numero, factorial);
+    return 0;
+}
